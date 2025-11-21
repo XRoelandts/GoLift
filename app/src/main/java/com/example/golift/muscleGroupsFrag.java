eaade88858eff9ca21b2a174f21493ecedbe140a
brockrossman <brock.rossman@gmail.com>
2025-11-20 20:55:09 -0600

    working on cards

--- app/src/main/java/com/example/golift/muscleGroupsFrag.java
+++ app/src/main/java/com/example/golift/muscleGroupsFrag.java
@@ -2,11 +2,13 @@ package com.example.golift;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
@@ -14,10 +16,21 @@ import android.widget.ListView;
 public class muscleGroupsFrag extends Fragment {
 
     ListView lv;
+    CardView cv1;
 
     public muscleGroupsFrag() {
         // Required empty public constructor
     }
+
+
+    // listener for when the card is clicked, to expand the listView
+    View.OnClickListener cv1Listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //lv.setVisibility(VISIBLE);
+
+        }
+    };
 
 
     @Override
@@ -34,8 +47,13 @@ public class muscleGroupsFrag extends Fragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_muscle_groups, container, false);
 
+        cv1 = view.findViewById(R.id.backCard);
+        cv1.setOnClickListener(cv1Listener);
+
         lv = view.findViewById(R.id.bicepLV);
-        // ListAdapter
+        // ArrayAdapter adapter = new ArrayAdapter<>(getActivity(), null, null);
+        // lv.setAdapter(adapter);
+
 
 
         return view;
