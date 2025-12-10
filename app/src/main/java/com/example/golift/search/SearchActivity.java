36b6891baec4adc294126aea7a24ef6dfdb11191
brockrossman <brock.rossman@gmail.com>
2025-12-10 04:30:11 -0600

    fixing SearchActivity 3.0 (should work a lot better) - Brock

--- app/src/main/java/com/example/golift/search/SearchActivity.java
+++ app/src/main/java/com/example/golift/search/SearchActivity.java
@@ -6,11 +6,15 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.FragmentManager;
@@ -20,25 +24,32 @@ import com.example.golift.R;
 import com.example.golift.pageButtonsFragment;
 import com.example.golift.saved.bookmarkedContentProvider;
 
-public class SearchActivity extends AppCompatActivity implements OnBookmarkButtonClickListener {
+public class SearchActivity extends AppCompatActivity {
 
     FragmentManager fg;
+
     gymContentProvider gymProvider;
+    SimpleCursorAdapter adapter;
+    gymsViewAdapter adapter1;
+
     bookmarkedContentProvider bookmarkProvider;
+
     Uri uri = gymContentProvider.CONTENT_URI;
+
+    Uri bookmarkUri = bookmarkProvider.CONTENT_URI;
 
     SearchView search;
     ListView gymView;
 
-    gymsViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_search);
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            // ... your insets code ...
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
@@ -47,50 +58,100 @@ public class SearchActivity extends AppCompatActivity implements OnBookmarkButto
             FragmentTransaction trans = fg.beginTransaction();
             pageButtonsFragment pageButtons = new pageButtonsFragment();
             trans.add(R.id.buttonFragments, pageButtons, "buttonsFrag");
+
             trans.commit();
         }
 
+        // Provider setup
         gymProvider = new gymContentProvider();
-        populateContent(); // Your method is called here
+
+        populateContent();
+
         bookmarkProvider = new bookmarkedContentProvider();
 
+        // Search setup
         search = findViewById(R.id.gymSearch);
+        search.clearFocus();
+        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // --- THIS IS THE NEW FILTERING LOGIC ---
+
+                // Define the selection criteria. The '?' is a placeholder.
+                // "LIKE ?" with "%" wildcards performs a "contains" search.
+                String selection = gymContentProvider.COL_NAME + " LIKE ?";
+                String[] selectionArgs = new String[]{"%" + newText + "%"};
+
+                // Re-query the ContentProvider with the new filter.
+                Cursor filteredCursor = getContentResolver().query(uri, null, selection, selectionArgs, null);
+
+                // Tell the adapter to use the new, filtered cursor.
+                // The adapter will automatically update the ListView.
+                adapter.changeCursor(filteredCursor);
+
+                return true; // We've handled the event
+            }
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                return false;
+            }
+        });
+
+        // List View setup
 
         gymView = findViewById(R.id.gymLV);
-        Cursor data = getContentResolver().query(uri, null, null, null, null);
 
-        String[] mListColumns = new String[]{gymContentProvider.COL_NAME, gymContentProvider.COL_DISTANCE};
-        int[] mListItems = new int[]{R.id.gymNameTV, R.id.gymDistTV};
+        Cursor data = getContentResolver().query(uri, null, null, null, null, null);
 
-        adapter = new gymsViewAdapter(this, R.layout.workoutscard, data, mListColumns, mListItems, 0, this);
+        if(data != null) {
+            data.moveToFirst();
+            Log.i("data test", data.getString(data.getColumnIndex("Name")));
+        }
 
-        gymView.setAdapter(adapter);
+        String[] mListColumns = new String[] { gymContentProvider.COL_NAME, gymContentProvider.COL_DISTANCE};
+        int[] mListItems = new int[] { R.id.gymNameTV, R.id.gymDistTV };
+
+        adapter = new SimpleCursorAdapter(getApplicationContext(), R.layout.workoutscard, data, mListColumns, mListItems );
+        adapter1 = new gymsViewAdapter(SearchActivity.this, R.layout.workoutscard, data, mListColumns, mListItems);
+
+        gymView.setAdapter(adapter1);
     }
 
-    @Override
-    public void onBookmarkClick(Cursor cursor) {
-        if (cursor == null) {
-            return;
+    public void bookmarkGym(String gymName, int position) {
+
+        Log.i("bookmark", "check");
+
+        Cursor data = getContentResolver().query(uri, null, null,
+                null, null, null);
+
+        Cursor data1 = getContentResolver().query(bookmarkUri, null, null,
+                null, null, null);
+
+        data.moveToPosition(position);
+
+
+        ContentValues values = new ContentValues();
+        values.put(bookmarkedContentProvider.COL_NAME, data.getString(data.getColumnIndex("Name")));
+        values.put(bookmarkedContentProvider.COL_DISTANCE, data.getString(data.getColumnIndex("Distance")));
+        values.put(bookmarkedContentProvider.COL_DESCRIPTION, data.getString(data.getColumnIndex("Description")));
+
+        // Use the ContentResolver to add the row.
+        getContentResolver().insert(bookmarkUri, values);
+
+        for(int i = 0; i < data1.getCount(); i++)
+        {
+            data1.moveToPosition(i);
+            Log.i("BookmarkDB", data1.getString(data.getColumnIndex("Name")));
         }
-        try {
-            String gymName = cursor.getString(cursor.getColumnIndexOrThrow(gymContentProvider.COL_NAME));
-            String gymDist = cursor.getString(cursor.getColumnIndexOrThrow(gymContentProvider.COL_DISTANCE));
 
-            Toast.makeText(this, "Bookmarked: " + gymName, Toast.LENGTH_SHORT).show();
 
-            ContentValues values = new ContentValues();
-            values.put(bookmarkedContentProvider.COL_NAME, gymName);
-            values.put(bookmarkedContentProvider.COL_DISTANCE, gymDist);
-
-            getContentResolver().insert(bookmarkedContentProvider.CONTENT_URI, values);
-
-        } catch (IllegalArgumentException e) {
-            Log.e("SearchActivity", "Error getting column from cursor: " + e.getMessage());
-        }
     }
 
 
-public void populateContent() {
+
+    public void populateContent() {
         Cursor data = getContentResolver().query(uri, null, null, null, null, null);
         if(data.getCount() == 0) {
             ContentValues values = new ContentValues();
--- app/src/main/java/com/example/golift/search/gymsViewAdapter.java
+++ app/src/main/java/com/example/golift/search/gymsViewAdapter.java
@@ -2,53 +2,50 @@ package com.example.golift.search;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton; // Use ImageButton
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import com.example.golift.R;
 
 
-interface OnBookmarkButtonClickListener {
-    void onBookmarkClick(Cursor cursor);
-}
+
 
 public class gymsViewAdapter extends SimpleCursorAdapter {
 
-    private final OnBookmarkButtonClickListener listener;
+
 
     // FIX #1: REMOVED the extra constructor. This is now the ONLY one.
-    public gymsViewAdapter(Context context, int layout, Cursor c, String[] from, int[] to, int flags, OnBookmarkButtonClickListener listener) {
-        super(context, layout, c, from, to, flags);
-        this.listener = listener;
+    public gymsViewAdapter(Context context, int layout, Cursor c, String[] from, int[] to) {
+        super(context, layout, c, from, to);
+
     }
 
-    /**
-     * This method is called for every row in the ListView.
-     * We override it to find our button and attach a click listener.
-     */
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        // Let the SimpleCursorAdapter do its job first to create/recycle the view
-        // and populate the TextViews (gymNameTV, gymDistTV).
-        View view = super.getView(position, convertView, parent);
+    public void bindView(View view, Context context, Cursor cursor) {
+        super.bindView(view, context, cursor);
+        TextView gymNameTV = view.findViewById(R.id.gymNameTV);
+        String gymName = gymNameTV.getText().toString();
 
-        // FIX #2: Change the type from Button to ImageButton to match the XML.
+        int position = cursor.getPosition();
+
+
         ImageButton bookmarkButton = view.findViewById(R.id.bookmarkButton);
 
-        // Get the cursor pointing to the data for the current row
-        Cursor cursor = (Cursor) getItem(position);
+        bookmarkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("OnClick", gymName);
+                if (context instanceof SearchActivity) {
+                    Log.i("context", "correct");
+                    ((SearchActivity) context).bookmarkGym(gymName, position);
+                }
 
-        if (bookmarkButton != null && listener != null) {
-            bookmarkButton.setOnClickListener(v -> {
-                // When the button is clicked, call the listener's method,
-                // passing the cursor for this row back to the Activity.
-                listener.onBookmarkClick(cursor);
-            });
-        }
+            }
+        });
 
-        // Also removed the extra semicolon from the end of the method
-        return view;
     }
 }
